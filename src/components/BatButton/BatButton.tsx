06f9d9f27d1d30434887b4847cceafcf22ac9edf
Juan Victor <estudantejuan0@gmail.com>
2025-08-16 15:11:07 -0300

    feat: implements project generate password theme batman

---
+++ src/components/BatButton/BatButton.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { View, Text, Pressable } from 'react-native';
+
+import { styles } from './BatButtonStyles';
+import { BatTextInput } from "../BatTextInput/BatTextInput";
+import generatePass from "../../services/passwordService";
+
+import * as ClipBoard from 'expo-clipboard';
+
+export function BatButton(){
+    const [pass, setPass] = useState('');
+
+    function handleGenerateButton(){
+        let generateToken = generatePass();
+        setPass(generateToken);
+    }
+
+    function handleCopyButton(){
+        ClipBoard.setStringAsync(pass);
+    }
+
+    return (
+        <>
+            <BatTextInput
+                pass={pass}
+            />
+            <Pressable
+                onPress={handleGenerateButton}
+                style={styles.button}
+            >
+                <Text style={styles.text}> GENERATE </Text>
+            </Pressable>
+
+            <Pressable
+                onPress={handleCopyButton}
+                style={styles.button}
+            >
+                <Text style={styles.text}>☀️ COPY </Text>
+            </Pressable>
+        </>
+    )
+}
